7986423c2b131b8200bfa25db6f63c3be2b078f0
Lokeshwarreddy76 <nandyalalokeshwarreddy@gmail.com>
2026-07-24 10:50:42 +0530

    Add files via upload

---
+++ Day-5/string operations part-1.py
@@ -0,0 +1,57 @@
+Python 3.13.14 (tags/v3.13.14:fd17997, Jun 10 2026, 13:03:48) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+s=''
+s
+''
+>>> s='codegnan'
+>>> s
+'codegnan'
+>>> 'codegnan' + 'PFS'
+'codegnanPFS'
+>>> 'codegnan'*10
+'codegnancodegnancodegnancodegnancodegnancodegnancodegnancodegnancodegnancodegnan'
+>>> '*'*20
+'********************'
+>>> '_*_'*20
+'_*__*__*__*__*__*__*__*__*__*__*__*__*__*__*__*__*__*__*__*_'
+>>> s = 'codegnan'
+>>> s[4]
+'g'
+>>> s[7]
+'n'
+>>> s[0]
+'c'
+>>> s[-1]
+'n'
+>>> s[-2]
+'a'
+>>> names = 'lokesh ganesh avinash bharath'
+>>> names[0]
+'l'
+>>> names[5]
+'h'
+>>> #s[start:end+1:step]=>s[0:len:1]
+>>> names[0:5]
+'lokes'
+>>> names[:6]
+'lokesh'
+>>> names
+'lokesh ganesh avinash bharath'
+>>> names[7:11]
+'gane'
+>>> names[13:20]
+' avinas'
+>>> names[21:]
+' bharath'
+>>> names[-1:-8:-1]
+'htarahb'
+>>> names[::2]
+'lks aehaiahbaah'
+>>> names
+'lokesh ganesh avinash bharath'
+>>> 'lokesh' in names
+True
+>>> 'avinash' in names
+True
+>>> 'vikas' not in names
+True
